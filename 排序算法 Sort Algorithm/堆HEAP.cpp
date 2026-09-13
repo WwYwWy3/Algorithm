95961f45f7dc7cd284d0707858ce48ec4e40aa39
WwYwWy3 <2442336594@qq.com>
2026-09-13 13:54:19 +0800

    Implement heap sort algorithm
    Refactored sorting algorithm to implement heap sort instead of quicksort. Added heap data structure and associated methods for building and managing the heap.

--- 排序算法 Sort Algorithm/堆HEAP.cpp
+++ 排序算法 Sort Algorithm/堆HEAP.cpp
@@ -1,45 +1,82 @@
 #include<iostream>
-#include<random>
-
-// std::mt19937 gen(std::random_device{}());
+#include <vector>
+#include <algorithm>
 
 bool cmp(int a, int b){
-	return a <= b;
+	return a < b;
 }
 
-int PARTATION(int arr[], int I, int J, bool (*cmp)(int a, int b)){
-	// std::uniform_int_distribution<int> dist(I, J);
-    // int p = dist(gen);          
-    // std::swap(arr[p], arr[I]);  
-    
-    int pivot = arr[I], l = I, r = J;
-	
-	while(l < r){
-		while(cmp(pivot, arr[r]) && l < r){r--;}
-		arr[l] = arr[r];
-		while(cmp(arr[l], pivot) && l < r){l++;}
-		arr[r] = arr[l];
-	}
-	arr[l] = pivot;
-	
-	return l;
-}
 
-void QUICKSORT(int arr[], int I, int J, bool (*cmp)(int a, int b)){
-	if(I >= J) return;
-	int pivot = PARTATION(arr, I, J, cmp);
-	QUICKSORT(arr, I, pivot - 1, cmp);
-	QUICKSORT(arr, pivot + 1, J, cmp);
+
+struct HEAP{
+    std::vector<int> heap;   
+    int max_size, cur_size;
+
+    static bool cmp(int a, int b){ return a < b; }   
+
+    HEAP(int arr[], int I, int J){
+        max_size = J - I + 10;
+        heap.assign(max_size + 1, 0);   
+        cur_size = 0;
+        BUILDHEAP(arr, I, J);
+    }
+
+    void UP(int cnt){
+        while(cnt > 1 && cmp(heap[cnt], heap[cnt/2])){
+            std::swap(heap[cnt], heap[cnt/2]);
+            cnt /= 2;
+        }
+    }
+
+    void DOWN(int cnt){
+        while(cnt * 2 <= cur_size){          
+            int t = cnt * 2;
+            if(t + 1 <= cur_size && cmp(heap[t + 1], heap[t])){t++;}  
+            if(!cmp(heap[t], heap[cnt])) break;                        
+            std::swap(heap[cnt], heap[t]);
+            cnt = t;
+        }
+    }
+
+    void BUILDHEAP(int arr[], int I, int J){
+        cur_size = 0;
+        for(int i = 1, j = I; j <= J; ++j, ++i){   
+            heap[i] = arr[j];
+            cur_size++;
+        }
+        for(int i = cur_size / 2; i >= 1; --i){    
+            DOWN(i);
+        }
+    }
+
+    int GETSIZE(){ return cur_size; }
+
+    void PUSH(int item){
+        heap[++cur_size] = item;
+        UP(cur_size);
+    }
+
+    void POP(){
+        if(cur_size == 0) return;
+        heap[1] = heap[cur_size];
+        cur_size--;
+        if(cur_size > 0) DOWN(1);
+    }
+
+    int TOP(){ return heap[1]; }
+};
+
+void HEAPSORT(int arr[], int I, int J){
+	HEAP heap(arr, I, J);
+	for(int i = I;i <= J; ++i){arr[i] = heap.TOP(); heap.POP();}
 }
 
 int main(){
-    int N;
-    std::cin >> N;
-	int arr[100010];
+	int arr[10];
 	
-	for (int i = 0;i < N; ++i) std::cin >> arr[i];
+	for (int i = 0;i < 10; ++i) std::cin >> arr[i];
 	
-	QUICKSORT(arr, 0, N - 1, &cmp);
+	HEAPSORT(arr, 0, 9);
 	
-	for (int i = 0;i < N; ++i) std::cout << arr[i] << ' ';
+	for (int i = 0;i < 10; ++i) std::cout << arr[i] << ' ';
 }
